dcfeb3e5669b975fa9c370fe4282296fb1c1c5cb
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-06 14:41:56 -0400

    Added instructions

--- src/App.js
+++ src/App.js
@@ -23,3 +23,10 @@ function App() {
 }
 
 export default App;
+
+// Mandatory Content
+
+// View all of my email messages (subject line + sender)
+// View one of my email messages with all of its details
+// Send an email
+// Search for a specific email by subject
