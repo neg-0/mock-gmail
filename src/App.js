48e3686ba6a5deea39fd0384623c1b25a90a39ca
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-06 14:56:33 -0400

    Added method to fetch emails

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+const url = "http://localhost:3001"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  componentDidMount() {
+    this.fetchEmails()
+  }
+
+  async fetchEmails() {
+    let response = await fetch(`${url}/emails`)
+    let json = await response.json()
+
+    console.log(json)
+  }
+
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
